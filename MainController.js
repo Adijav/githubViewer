1e5bb75a655524f11ea187b01d92e96f08c8cf6d
Adijav <agjavali@gmail.com>
2016-06-09 00:33:49 -0700

    added github viewer app

---
+++ MainController.js
@@ -0,0 +1,14 @@
+(function(){
+	var app = angular.module("githubViewer");
+
+
+	var MainController = function($scope,$location){
+
+		$scope.search = function(username){
+			$location.path("/user/"+username);
+		};	
+	};
+
+	app.controller("MainController",MainController);
+
+})();
